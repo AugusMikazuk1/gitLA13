e8e4f3beee154bf5ee0c1b1372271440c0f5d05d
John Kenneth Gutierrez <johnkengutierrez@gmail.com>
2024-12-17 23:46:10 +0800

    program with view record

--- program.py
+++ program.py
@@ -16,3 +16,9 @@ print("Clear All Records")
 clearRec() 
 elif choice.upper() == 'D': 
 print("Thank you!")
+
+def viewRec():
+    file = open(filename, 'r')
+    with open(filename, 'r') as read_file:
+        print(read_file.read())
+    read_file.close()
